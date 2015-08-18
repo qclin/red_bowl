d9abb1e69e52e8ebe87c30e34d3d38c6815fed3b
Qiao Lin <qiaoclin@gmail.com>
2015-08-18 15:23:30 -0400

    csrf protection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,7 @@
 //= require turbolinks
 //= require angular
 //= require angular-devise
-//= require angular-devise/lib/devise
+//= require angular-rails-csrf
 //= require angular-ui-router
 //= require angular-rails-templates
 //= require_tree .
