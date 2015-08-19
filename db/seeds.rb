3e08aded90c8b5f69965715c52c182e212ae2e9a
Qiao Lin <qiaoclin@gmail.com>
2015-08-19 17:14:41 -0400

    db:seed with admin & contest

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+admin = User.create(username:'admin', email: 'admin@gameofbowls.com', password: 'yoroshiku'); 
+
+Contest.create([{category: 'kawaii desu', deadline: "Jan-15-2015"}, 
+    {category: 'flatiron toast', deadline: "Feb-15-2015"}, 
+    {category: 'nori wrap', deadline: "Mar-15-2015"}, 
+    {category: 'baby it is cold outside', deadline: "Apr-15-2015"}, 
+    {category: 'berlin berg', deadline: "May-15-2015"}, 
+    {category: 'kuntshall ', deadline: "Jun-15-2015"}, 
+    {category: 'words word', deadline: "Jul-15-2015"}, 
+    {category: 'summertime sadness', deadline: "Aug-15-2015"}, 
+    {category: 'tuna melt', deadline: "Sep-15-2015"}
+    ]); 
+
